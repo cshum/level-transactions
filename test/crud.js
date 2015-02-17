23820d4a511a62e8ec4e8787bcc0687f2608fd15
Adrian C Shum <adrian@cshum.com>
2015-02-17 20:09:43 +0800

    tweak test case

--- test/crud.js
+++ test/crud.js
@@ -3,7 +3,7 @@ var tape         = require('tape'),
     down         = require('memdown'),
     transactions = require('../');
 
-tape('Locking',function(t){
+tape('Locking and Isolation',function(t){
   t.plan(12);
 
   var db = levelup('test', {
