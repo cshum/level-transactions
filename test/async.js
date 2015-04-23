88be95b9043452c14769f149e0d7c32929dd435b
Adrian Shum <me@cshum.com>
2015-04-23 12:56:32 +0800

    break stuff again

--- test/async.js
+++ test/async.js
@@ -15,12 +15,13 @@ tape('Async Lock',function(t){
   var tx = db.transaction();
   var tx2 = db.transaction();
 
+  tx.put('k', 167);
+
   tx2.get('k', function(err, value){
     t.equal(value, 167, 'tx2 get equals 167');
     tx2.put('k', value+1);
   });
 
-  tx.put('k', 167);
   tx.commit();
 
   tx2.commit(function(err){
@@ -28,7 +29,34 @@ tape('Async Lock',function(t){
       t.equal(value, 168, 'db get equals 168');
     });
   });
+});
 
+tape('Async Lock',function(t){
+  t.plan(2);
+
+  var db = levelup('test', {
+    db: down,
+    valueEncoding: 'json'
+  });
+  transactions(db);
+
+  var tx = db.transaction();
+  var tx2 = db.transaction();
+
+  tx.put('k', 167);
+  tx.commit();
+
+  tx2.get('k', function(err, value){
+    t.equal(value, 167, 'tx2 get equals 167');
+
+    tx2.put('k', value+1);
+
+    tx2.commit(function(err){
+      db.get('k', function(err, value){
+        t.equal(value, 168, 'db get equals 168');
+      });
+    });
+  });
 
 });
 
